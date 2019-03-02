c61d93c2deb47b49f7f21e30447d3ca1e798f799
Rylan <rylanoc@gmail.com>
2019-03-02 01:29:26 -0500

    add skeleton code

--- __init__.py
+++ __init__.py
@@ -1,9 +1,23 @@
 from binaryninja import *
 
+# https://www.cs.ucsb.edu/~yufeiding/cs293s/slides/293S_07_SSA_dead.pdf (for worklist style algorithm)
+
 # driver function
 def find_dead_code(bv,function):
 
 
-# check 
-def is_critical()
+# check if a basic block contains critical edges
+# (edges from a basic block with multiple successors to one with multiple predecessors)
+def is_critical(bb):
+
+
+# worklist style function to recursively find all dead code
+def mark(bv):
+
+
+# delete redundant code (or modify destinations in the case of jumps)
+def sweep(bv, marks):
+
+
+
 PluginCommand.register_for_address("Dead Code Eliminator", "Detects and eliminates dead code in the current binary", find_dead_code())
